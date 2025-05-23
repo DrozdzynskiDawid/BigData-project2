cb84ebd077dcc514654bd80fb043c3884680c520
Dawid <dawid.drozdzynski.g4@gmail.com>
2025-05-23 19:45:40 +0200

    update README

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-pip install kafka-python
+# BigData - project2
+**Platforma: Flink (DataStream API)**
 
+**Zestaw 1 – Netflix-Prize-Data**
 
-``` bash
+### Instrukcja uruchamiania projektu:
+- pobierz dane dla zestawu 1 i umieść je w folderze data
+- zainstaluj potrzebne biblioteki, w szczególności: `pip install kafka-python`
+
+- ustaw wartości poszczególnych parametrów skryptu zgodnie ze swoją konfiguracją:
+```
+CSV_FOLDER = 'data\\netflix-prize-data'
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+KAFKA_TOPIC = 'netflix'
+DELAY_SECONDS = 1 
+```
+
+- utwórz temat producenta w kontenerze Kafki:
+ ``` bash
 /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server kafka:9092 \
  --replication-factor 1 --partitions 3 --topic netflix
 ```
-
-uruchomić skrypt kafka_producer.py
+- uruchom skrypt kafka_producer.py i obserwuj temat producenta
--- kafka_producer.py
+++ kafka_producer.py
@@ -3,30 +3,27 @@ import csv
 import time
 from kafka import KafkaProducer
 
-# === KONFIGURACJA ===
-CSV_FOLDER = 'data\\netflix-prize-data'  # Folder z plikami CSV
+CSV_FOLDER = 'data\\netflix-prize-data'
 KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
 KAFKA_TOPIC = 'netflix'
-DELAY_SECONDS = 1  # opóźnienie między rekordami (symulacja strumienia)
+DELAY_SECONDS = 1 
 
-# === PRODUCENT KAFKA ===
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-    value_serializer=lambda v: v.encode('utf-8')  # serializacja tekstowa
+    value_serializer=lambda v: v.encode('utf-8')
 )
 
 def send_csv_file(file_path):
-    print(f"➡️ Wysyłanie z pliku: {file_path}")
+    print(f"Wysyłanie z pliku: {file_path}")
     with open(file_path, mode='r', newline='') as csvfile:
         reader = csv.reader(csvfile)
-        header = next(reader, None)  # pomiń nagłówek, jeśli jest
+        header = next(reader, None)
 
         for row in reader:
             date, film_id, user_id, rate = row
             message = f"{date},{film_id},{user_id},{rate}"
-            print(message)
             producer.send(KAFKA_TOPIC, message)
-            print(f"   📨 Wysłano: {message}")
+            print(f"Wysłano: {message}")
             time.sleep(DELAY_SECONDS)
 
 def produce_from_folder(folder_path):
@@ -36,7 +33,7 @@ def produce_from_folder(folder_path):
             send_csv_file(full_path)
 
     producer.flush()
-    print("✅ Zakończono wysyłanie danych z folderu.")
+    print("Zakończono wysyłanie danych z folderu.")
 
 if __name__ == '__main__':
     produce_from_folder(CSV_FOLDER)
